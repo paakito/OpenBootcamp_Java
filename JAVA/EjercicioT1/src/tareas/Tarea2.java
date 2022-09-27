786050e0ea2549a3417aae6d1843b2d759b32291
Paco <105666446+paakito@users.noreply.github.com>
2022-09-27 13:18:54 +0200

    Add files via upload

---
+++ JAVA/EjercicioT1/src/tareas/Tarea2.java
@@ -0,0 +1,33 @@
+package tareas;
+
+public class Tarea2 {
+
+    public static void main(String[] args) {
+
+        // NUMERICOS \\
+
+        byte var1 = 1;
+        short var2 = 2;
+        int var3 = 3;
+        long var4 = 4;
+
+
+        // ENTEROS \\
+
+        float var5 = 1.0f;
+        double var6 = 2.0d;
+
+
+        // BOOLEANO \\
+
+        boolean var7 = false;
+        boolean var8 = true;
+
+
+        // TEXTO \\
+
+        char var9 = 't';
+        String var10 = "Txt de Prueba";
+
+    }
+}
